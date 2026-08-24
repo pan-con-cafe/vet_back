945f89efa649c93dd55e996b509a4ae1f18fe4df
Pancito <pansiendopan@gmail.com>
2026-08-24 16:42:02 -0500

    Replace TypeVacuna_FK with TypeName property

--- vet_domain/DTOs/VacunaDTO.cs
+++ vet_domain/DTOs/VacunaDTO.cs
@@ -9,7 +9,7 @@ namespace vet_domain.DTOs
     public class VacunaDTO
     {
         public int Mascota_FK { get; set; }
-        public int TypeVacuna_FK { get; set; }
+        public string? TypeName { get; set; }
         public DateOnly Date { get; set; }
         public decimal? Weight { get; set; }
         public decimal? Temperature { get; set; }
